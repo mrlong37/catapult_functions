de9a4f11bc24b8f48d2c3fa24c596fe1fd01c637
Lucas Matte <lmatte7@gmail.com>
2024-06-19 20:24:33 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let button = document.querySelector(".password-check");
+
+// 1 - Write your function here
+// Write a function to test if the password is correct
+// The function should take in the text of the password
+// as an input, and return true if the password is correct
+
+
+button.addEventListener("click", function() {
+
+    let password = document.querySelector(".password-input").value;
+
+    // 2 - Call your function here, store the result in a variable
+    // and use the variable to determine which message to display
+    if () {
+        document.querySelector(".yes").style = "display:block";
+        document.querySelector(".denied").style = "display:none";
+    } else {
+        document.querySelector(".denied").style = "display:block";
+        document.querySelector(".yes").style = "display:none";
+    }
+
+});
+
